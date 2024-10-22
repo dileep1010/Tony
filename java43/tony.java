5261299cc07a26694d8798485b2077a469860da8
dileep1010 <228a1a4443@risekrishnasaiprakasam.edu.in>
2024-10-22 15:50:55 +0530

    Update tony.java

--- java43/tony.java
+++ java43/tony.java
@@ -1,7 +1,5 @@
-class Dileep
-{
-	public static void main(String args[])
-	{
-		System.out.println("Tony");
-	}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
